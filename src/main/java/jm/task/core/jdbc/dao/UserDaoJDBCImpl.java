107e0a6ade1cc9e07aed5e217c8f97ae2b40e6f2
AnDr10wA <45951252+AnDr10wA@users.noreply.github.com>
2023-05-30 22:56:01 +0300

    result 1.1.4 part 1

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,102 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
+
+
+    PreparedStatement statment = null;
+    String sql;
+
     public UserDaoJDBCImpl() {
 
     }
+    private  void Conn(String sql){
+        Util util = new Util();
+        Connection connection = util.getConnection();
+        try {
+            statment = connection.prepareStatement(sql);
+            statment.executeUpdate();
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            if (statment != null){
+                try {
+                    statment.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            if (connection != null){
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
 
     public void createUsersTable() {
+        sql = "CREATE TABLE Users( ID int NOT NULL AUTO_INCREMENT, name varchar(100), lastname varchar(100),age INT, " +
+                "PRIMARY KEY (ID));";
+        Conn(sql);
 
     }
 
     public void dropUsersTable() {
-
+        sql = "DROP TABLE users";
+        Conn(sql);
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        sql = "INSERT INTO users (name, lastname, age) VALUES (\""
+                 + name + "\",\"" +  lastName +  "\"," + age + ");";
+        Conn(sql);
 
     }
 
     public void removeUserById(long id) {
-
+        sql = "delete from users where id = " + id;
+        Conn(sql);
     }
 
     public List<User> getAllUsers() {
-        return null;
+        Util util = new Util();
+        Connection connection = util.getConnection();
+        sql = "select * from users";
+        ArrayList<User> usersList = new ArrayList<>();
+        try {
+            Statement statement = connection.createStatement();
+            ResultSet resultset = statement.executeQuery(sql);
+            while (resultset.next()){
+                User user = new User();
+                user.setId(resultset.getLong(1));
+                user.setName(resultset.getString(2));
+                user.setLastName(resultset.getString(3));
+                user.setAge(resultset.getByte(4));
+                usersList.add(user);
+
+            }
+
+        } catch ( SQLException e){
+            e.printStackTrace();
+        }
+        System.out.println(usersList);
+
+
+        return usersList;
     }
 
     public void cleanUsersTable() {
-
+        sql = "DELETE FROM users;";
+        Conn(sql);
     }
 }
--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,31 +1,34 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
+    UserDaoJDBCImpl daouser = new UserDaoJDBCImpl();
     public void createUsersTable() {
-
+        daouser.createUsersTable();
     }
 
     public void dropUsersTable() {
-
+        daouser.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-
+        daouser.saveUser(name,  lastName, age);
     }
 
     public void removeUserById(long id) {
-
+        daouser.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-        return null;
+
+        return daouser.getAllUsers();
     }
 
     public void cleanUsersTable() {
-
+            daouser.createUsersTable();
     }
 }
